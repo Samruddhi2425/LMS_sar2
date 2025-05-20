86cab463a9fee5833371c719d857ffb62d957a5c
AvadatiPendurkar <avadatipendurkar@gmail.com>
2025-05-20 18:47:33 +0530

    api for get books service generated

--- LMS/src/app/app.component.ts
+++ LMS/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HomeComponent } from './home_/home/home.component';
+// import { HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
--- LMS/src/app/home_/home/home.component.ts
+++ LMS/src/app/home_/home/home.component.ts
@@ -1,19 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, NgModule, OnInit } from '@angular/core';
 import { NavbarComponent } from '../navbar/navbar.component';
 import { GetbooksService } from '../../service/getbooks.service';
 import { BookdescriptionComponent } from '../bookdescription/bookdescription.component';
 import { RouterModule } from '@angular/router';
+// import { CommonModule } from '@angular/common';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 
 
 
 @Component({
   selector: 'app-home',
-  imports: [NavbarComponent,GetbooksService],
+  imports: [NavbarComponent,HttpClientModule,NgModule],
 
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrl: './home.component.css',
+  providers:[GetbooksService]
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit{
 books: any[]=[];
 constructor(private getBookService: GetbooksService){}
 
@@ -21,6 +24,7 @@ ngOnInit(): void {
   this.getBookService.getBooks().subscribe(
     (data) => {
       this.books = data;
+      console.log(data);
     },
     (error) => {
       console.error('Error fetching books:',error);
