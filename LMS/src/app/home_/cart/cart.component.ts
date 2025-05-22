808cbe23d173ee2bd9bdae95b2e0661f756dc174
Srohit90 <rohitshinde43434@gmail.com>
2025-05-22 15:10:12 +0530

    cart modify

--- LMS/src/app/home_/cart/cart.component.ts
+++ LMS/src/app/home_/cart/cart.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { NavbarComponent } from '../navbar/navbar.component';
 
 @Component({
   selector: 'app-cart',
-  imports: [],
+  imports: [NavbarComponent],
   templateUrl: './cart.component.html',
   styleUrl: './cart.component.css'
 })
