a8bf4d111643c62a73216aeb39e979b07a2902f3
Samruddhi Sarvankar <samruddhi25900@gmail.com>
2025-05-22 15:19:52 +0530

    added component

--- LMS/src/app/app.routes.ts
+++ LMS/src/app/app.routes.ts
@@ -2,11 +2,15 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './home_/home/home.component';
 import { GenreComponent } from './genre/genre.component';
 import { CartComponent } from './home_/cart/cart.component';
+import { DashboardComponent } from './manager/dashboard/dashboard.component';
+import { UserComponent } from './userProfile/user.component';
 
 export const routes: Routes = [
     {path:"",component:HomeComponent},
     {path:"home",component:HomeComponent},
     {path:"genre", component:GenreComponent},
-    {path:"cart", component:CartComponent}
+    {path:"cart", component:CartComponent},
+    {path:"managerdashboard",component:DashboardComponent},
+    {path:"user",component:UserComponent}
 
 ];
