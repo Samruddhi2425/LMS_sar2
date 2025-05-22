1280edf30395c6ac29f4e08003c034659824e969
AvadatiPendurkar <avadatipendurkar@gmail.com>
2025-05-22 12:03:45 +0530

    change

--- LMS/src/app/cart/cart.component.ts
+++ LMS/src/app/cart/cart.component.ts
@@ -1,8 +1,10 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-cart',
-  imports: [],
+  imports: [FormsModule,CommonModule],
   templateUrl: './cart.component.html',
   styleUrl: './cart.component.css'
 })
