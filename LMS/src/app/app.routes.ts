1d153abaed72a98c22292ab653724ec8ee1afe55
charlie2307 <riteshalim237@gmail.com>
2025-05-22 16:28:11 +0530

    approut

--- LMS/src/app/app.routes.ts
+++ LMS/src/app/app.routes.ts
@@ -4,6 +4,9 @@ import { GenreComponent } from './genre/genre.component';
 import { CartComponent } from './home_/cart/cart.component';
 import { DashboardComponent } from './manager/dashboard/dashboard.component';
 import { UserComponent } from './userProfile/user.component';
+import { ManagerComponent } from './manager/manager/manager.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
 export const routes: Routes = [
     {path:"",component:HomeComponent},
