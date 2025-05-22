7e5f31694e56695c34805f2a2e40025a137729d1
Srohit90 <rohitshinde43434@gmail.com>
2025-05-22 17:01:16 +0530

    changes

--- LMS/src/app/genre/genre.component.ts
+++ LMS/src/app/genre/genre.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from '../home_/navbar/navbar.component';
+import { BookdescriptionComponent } from '../home_/bookdescription/bookdescription.component';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-genre',
-  imports: [NavbarComponent],
+  imports: [NavbarComponent,BookdescriptionComponent,RouterModule,NavbarComponent],
   templateUrl: './genre.component.html',
   styleUrl: './genre.component.css'
 })
