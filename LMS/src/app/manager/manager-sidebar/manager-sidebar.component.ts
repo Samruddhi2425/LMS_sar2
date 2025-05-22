5de9072adbd476082c3af63483f868524e622e7b
charlie2307 <riteshalim237@gmail.com>
2025-05-22 16:14:53 +0530

    manager

---
+++ LMS/src/app/manager/manager-sidebar/manager-sidebar.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-manager-sidebar',
+  imports: [RouterOutlet],
+  templateUrl: './manager-sidebar.component.html',
+  styleUrl: './manager-sidebar.component.css'
+})
+export class ManagerSidebarComponent {
+
+}
