87cbc222886815ec1e09df02ab03ac12e20a17e2
AvadatiPendurkar <avadatipendurkar@gmail.com>
2025-05-24 15:55:25 +0530

    user

--- LMS/src/app/userProfile/user.component.ts
+++ LMS/src/app/userProfile/user.component.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { NavbarComponent } from '../home_/navbar/navbar.component';
+import { GetusersService } from '../service/getusers.service';
 
 @Component({
   selector: 'app-user',
@@ -8,6 +9,21 @@ import { NavbarComponent } from '../home_/navbar/navbar.component';
   templateUrl: './user.component.html',
   styleUrl: './user.component.css'
 })
+interface Users{
+  userId:number;
+  firstName:string;
+  lastName:string;
+  email:string;
+  pass:string;
+  mobileNo:string;
+}
+
 export class UserComponent {
+users:any[]=[];
+constructor(private getUserService: GetusersService){}
+
+}
+
+
 
 }
