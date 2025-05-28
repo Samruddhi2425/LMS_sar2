620616fb1fba0978a5480493a15a5cf65bc7f222
Srohit90 <rohitshinde43434@gmail.com>
2025-05-28 18:52:08 +0530

    manager

--- LMS/src/app/manager/managersidebar/managersidebar.component.ts
+++ LMS/src/app/manager/managersidebar/managersidebar.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
-import { MAddBookComponent } from '../m-add-book/m-add-book.component';
-import { MViewBookComponent } from '../m-view-book/m-view-book.component';
-import { MViewUserComponent } from '../m-view-user/m-view-user.component';
+
 
 @Component({
   selector: 'app-managersidebar',
-  imports: [RouterModule, MViewBookComponent, MAddBookComponent,MViewUserComponent],
+  imports: [RouterModule, FormsModule],
   templateUrl: './managersidebar.component.html',
   styleUrl: './managersidebar.component.css'
 })
