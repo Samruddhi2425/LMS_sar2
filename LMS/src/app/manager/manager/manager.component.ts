3fcb07352b0ddadb2d381da1c8040ac1afdd4794
Srohit90 <rohitshinde43434@gmail.com>
2025-05-28 10:24:29 +0530

    manager dashboard

--- LMS/src/app/manager/manager/manager.component.ts
+++ LMS/src/app/manager/manager/manager.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { ManagersidebarComponent } from '../managersidebar/managersidebar.component';
 
 @Component({
   selector: 'app-manager',
-  imports: [],
+  imports: [RouterOutlet, ManagersidebarComponent],
   templateUrl: './manager.component.html',
   styleUrl: './manager.component.css'
 })
