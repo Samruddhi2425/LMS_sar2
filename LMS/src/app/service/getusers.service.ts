fbaf8eeb41fde719884b3743d2579032d4f4d25a
AvadatiPendurkar <avadatipendurkar@gmail.com>
2025-05-30 15:33:52 +0530

    getUserService

--- LMS/src/app/service/getusers.service.ts
+++ LMS/src/app/service/getusers.service.ts
@@ -20,4 +20,11 @@ export class GetusersService {
     return this.http.post<any[]>(this.baseUrl + "/register", userData);
     
   }
+
+  userBlock(userId:number):Observable<any[]>{
+    return this.http.put<any[]>(`https://localhost:7252/api/Users/block/${userId}`,{});
+  }
+  userUnBlock(userId: number):Observable<any[]>{
+    return this.http.put<any[]>(`https://localhost:7252/api/Users/unblock/${userId}`, {});
+
 }
