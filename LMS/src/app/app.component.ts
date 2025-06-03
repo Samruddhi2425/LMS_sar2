288dd0a092a833207c657a512b2de20e7cb355aa
Srohit90 <rohitshinde43434@gmail.com>
2025-06-03 16:51:18 +0530

    forget password

--- LMS/src/app/app.component.ts
+++ LMS/src/app/app.component.ts
@@ -4,11 +4,12 @@ import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
 // import { HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,RegisterComponent,CommonModule],
+  imports: [RouterOutlet,RegisterComponent,CommonModule,ReactiveFormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
