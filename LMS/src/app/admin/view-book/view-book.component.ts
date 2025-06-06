dbadd52876bbe0598fc5abaf3e45e689c3f47158
AvadatiPendurkar <avadatipendurkar@gmail.com>
2025-06-06 16:28:43 +0530

    report generate

--- LMS/src/app/admin/view-book/view-book.component.ts
+++ LMS/src/app/admin/view-book/view-book.component.ts
@@ -1,12 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 import { GetbooksService } from '../../service/getbooks.service';
 import { CommonModule } from '@angular/common';
-import { Route,RouterModule, RouterOutlet } from '@angular/router';
+import { Route, RouterModule, RouterOutlet } from '@angular/router';
 
 import { IssuebooksService } from '../../service/issuebooks.service';
 import { UserComponent } from '../userProfile/user.component';
 import { UpdateBookComponent } from '../update-book/update-book.component';
 import { FormsModule } from '@angular/forms';
+import jsPDF from 'jspdf';
+import autoTable from 'jspdf-autotable';
+
 
 
 interface Books {
@@ -21,82 +24,99 @@ interface Books {
 
 @Component({
   selector: 'app-view-book',
-  imports: [CommonModule,RouterModule,UserComponent,UpdateBookComponent, FormsModule], 
+  imports: [CommonModule, RouterModule, UserComponent, UpdateBookComponent, FormsModule],
   templateUrl: './view-book.component.html',
-  
-  providers:[GetbooksService],
+
+  providers: [GetbooksService],
   styleUrl: './view-book.component.css'
 })
-export class ViewBookComponent implements OnInit{
-  books:any[]=[];
-  issueBooks:any[]=[];
+export class ViewBookComponent implements OnInit {
+  books: any[] = [];
+  issueBooks: any[] = [];
   returnedBooks: any[] = [];
   searchTerm: string = '';
-allBooks: any[] = [];
+  allBooks: any[] = [];
 
-  constructor(private getBookService: GetbooksService, 
-            private getIssueService: IssuebooksService,
-            ){}
+  constructor(private getBookService: GetbooksService,
+    private getIssueService: IssuebooksService,
+  ) { }
 
   ngOnInit(): void {
     this.getBookService.getBooks().subscribe(
       (data) => {
         this.books = data;
-         this.allBooks = data; 
+        this.allBooks = data;
         console.log(data);
       },
       (error) => {
         console.error('Error fetching books:', error);
       }
     );
-     this.getIssueService.getIssuBook().subscribe(
-      (issData)=>{
-      this.issueBooks = issData;
-      this.returnedBooks = issData.filter(book => book.status === 'returned');
-      console.log(this.returnedBooks);
-      console.log(issData);
-    },
-    (error)=>{
-    console.error('Error while feting issue data');
-    }
-  );
-
+    this.getIssueService.getIssuBook().subscribe(
+      (issData) => {
+        this.issueBooks = issData;
+        this.returnedBooks = issData.filter(book => book.status === 'returned');
+        console.log(this.returnedBooks);
+        console.log(issData);
+      },
+      (error) => {
+        console.error('Error while feting issue data');
+      }
+    );
   }
-// goToUpdateBook(bookId:number): void{
+  // goToUpdateBook(bookId:number): void{
 
-// //  this.router.navigate(['/admin/viewBooks/update_book', bookId]);
-// }
-scrollTo(id: string) {
-  setTimeout(() => { // Small timeout to ensure fragment is processed
-    const element = document.getElementById(id);
-    if (element) {
-      element.scrollIntoView({ behavior: 'smooth' });
-    }
-  }, 100);
-}
-
-onSearch(event: Event): void {
-  event.preventDefault(); // Prevent form reload
-  const term = this.searchTerm.toLowerCase();
-  this.books = this.allBooks.filter(book =>
-    book.bookName.toLowerCase().includes(term) ||
-    book.authorName.toLowerCase().includes(term) ||
-    book.isbn.toLowerCase().includes(term) ||
-    book.genre.toLowerCase().includes(term)
-  );
-}
-onSearchInputChange(): void {
-  if (this.searchTerm.trim() === '') {
-    this.books = [...this.allBooks]; // Reset the full book list
+  // //  this.router.navigate(['/admin/viewBooks/update_book', bookId]);
+  // }
+  scrollTo(id: string) {
+    setTimeout(() => { // Small timeout to ensure fragment is processed
+      const element = document.getElementById(id);
+      if (element) {
+        element.scrollIntoView({ behavior: 'smooth' });
+      }
+    }, 100);
   }
-}
-clearSearch(): void {
-  this.searchTerm = '';
-  this.books = [...this.allBooks]; // Reset book list
 
+  onSearch(event: Event): void {
+    event.preventDefault(); // Prevent form reload
+    const term = this.searchTerm.toLowerCase();
+    this.books = this.allBooks.filter(book =>
+      book.bookName.toLowerCase().includes(term) ||
+      book.authorName.toLowerCase().includes(term) ||
+      book.isbn.toLowerCase().includes(term) ||
+      book.genre.toLowerCase().includes(term)
+    );
+  }
+  onSearchInputChange(): void {
+    if (this.searchTerm.trim() === '') {
+      this.books = [...this.allBooks]; // Reset the full book list
+    }
+  }
+  clearSearch(): void {
+    this.searchTerm = '';
+    this.books = [...this.allBooks]; // Reset book list
+  }
 
+  downloadPDF() {
+    const doc = new jsPDF();
 
+    doc.text('Pending Book Returns', 14, 15);
 
+    const head = [['Order ID', 'Book ID', 'Order Date']];
+    const data = this.issueBooks.map(book => [
+      book.issueId,
+      book.bookId,
+      book.issueDate
+    ]);
 
-}
+    autoTable(doc, {
+      head: head,
+      body: data,
+      startY: 20,
+      theme: 'grid',
+      headStyles: { fillColor: [0, 0, 0] }
+    });
+
+    doc.save('pending-returns.pdf');
+  }
 }
